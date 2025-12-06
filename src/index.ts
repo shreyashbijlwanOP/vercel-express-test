a9e66b8ddfb62e0e81f86001214150185fdad92e
Shreyash Bijlwan <shreyash.bijlwanmail@gmail.com>
2025-12-06 22:38:14 +0530

    added hello route

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,10 @@ app.get("/", (req, res) => {
   res.json([{ message: "hello this is from vercel server" }]);
 });
 
+app.get("/hello/:name", (req, res) => {
+  res.json({ message: `hello ${req.params.name || "user"}` });
+});
+
 app.listen(4000, () => {
   console.log("server is liting on port 4000");
 });
-    
